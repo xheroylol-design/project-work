e95f243d66e7c9fbc4cd2d971f5972fce19b1e99
xheroylol-design <xheroylol@gmail.com>
2026-04-08 01:45:13 +0600

    Expose helloSpeaker to the global scope

--- module4-solution/SpeakHello.js
+++ module4-solution/SpeakHello.js
@@ -21,4 +21,4 @@ function speak(name) {
 // 'helloSpeaker' on the global scope as well.
 // See Lecture 52, part 2
 // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
-xxxx.xxxx = helloSpeaker;
+window.helloSpeaker = helloSpeaker;
